1550407a25237743eb75e6dd5c12de06436f9942
Maha <maha.spearmint@gmail.com>
2022-08-22 17:34:26 +0800

    Added controllers js files and handlebars files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const auth = require('./web/auth');
-const blog = require('./web/blog');
-
-router.use(auth);
-router.use(blog);
-
-module.exports = router;
